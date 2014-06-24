559a271cc77f1cf55b0f8fca55e7dacbfdcb38db
Chelsea Voss <csvoss@mit.edu>
2014-06-24 14:56:09 -0400

    Modified aggregation script: select two date fields

--- calendar/sum_starters.py
+++ calendar/sum_starters.py
@@ -3,30 +3,36 @@
 
 import csv
 
-inputfilename = "../HMXPC13_DI_v2_5-14-14.csv"
-delimiter = ","
+inputfilename = '../HMXPC13_DI_v2_5-14-14.csv'
+delimiter = ','
 quotechar = '|'
 
-outputfilename = "summed_startdates.csv"
+fields = ['start_time_DI', 'last_event_DI']
+
+outputfilename = 'summed_startdates.csv'
 
 with open(inputfilename, 'rb') as csv_in:
     csvreader = csv.DictReader(csv_in, delimiter=delimiter, quotechar=quotechar)
 
-    sums = {}
+    sums_by_field = dict([(fi, {}) for fi in fields]) ## dictionary (by field) of dictionaries (by date)
 
     def is_date(string):
         return string.count('-')==2
 
     for row in csvreader:
-        date = row["start_time_DI"]
-        if is_date(date):
-            if date in sums:
+        for fi in fields:
+            date = row[fi]
+            if is_date(date):
+                sums = sums_by_field[fi]
+                if not date in sums:
+                    for fi in fields:
+                        sums_by_field[fi][date] = 0
                 sums[date] += 1
-            else:
-                sums[date] = 1
 
     with open(outputfilename, 'wb') as csv_out:
         csvwriter = csv.writer(csv_out, delimiter=delimiter, quotechar=quotechar, quoting=csv.QUOTE_MINIMAL)
-        csvwriter.writerow(['Date', 'Sum'])
+        csvwriter.writerow(['Date'] + fields)
         for date in sums.keys():
-            csvwriter.writerow([date, sums[date]])
+            csvwriter.writerow([date] +[sums_by_field[fi][date] for fi in fields])
+
+raw_input()
